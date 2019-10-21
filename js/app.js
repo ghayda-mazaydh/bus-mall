f9329527e553e6abb7cd121d6adaae4e2add1ab6
Your Name In Single Quotes <your email here in single quotes>
2019-10-21 14:48:34 +0300

    lab 11 done

--- js/app.js
+++ js/app.js
@@ -1,28 +1,28 @@
 'use strict'
 function Products(title, src) {
-    this.title = title;
-    this.src = src;
-    this.clickCounter = 0;
-    this.shownCounter = 0;
-    Products.all.push(this);
-  }
-  Products.roundCounter = 0;
-  Products.roundVotingLimit = 25;
-  Products.all = [];
+  this.title = title;
+  this.src = src;
+  this.clickCounter = 0;
+  this.shownCounter = 0;
+  Products.all.push(this);
+}
+Products.roundCounter = 0;
+Products.roundVotingLimit = 25;
+Products.all = [];
 
-  Products.container = document.getElementById('busMall-container');
-  
-  Products.firstImage = document.getElementById('first-image');
-  Products.secondImage = document.getElementById('second-image');
-  Products.thirdImage = document.getElementById('third-image');
-  
-  Products.firstImageTitle = document.getElementById('first-image-title');
-  Products.secondImageTitle = document.getElementById('second-image-title');
-  Products.thirdImageTitle = document.getElementById('third-image-title');
+Products.container = document.getElementById('busMall-container');
 
-  Products.firstObject = null;
-  Products.secondObject = null;
-  Products.thirdObject = null;
+Products.firstImage = document.getElementById('first-image');
+Products.secondImage = document.getElementById('second-image');
+Products.thirdImage = document.getElementById('third-image');
+
+Products.firstImageTitle = document.getElementById('first-image-title');
+Products.secondImageTitle = document.getElementById('second-image-title');
+Products.thirdImageTitle = document.getElementById('third-image-title');
+
+Products.firstObject = null;
+Products.secondObject = null;
+Products.thirdObject = null;
 
 new Products('bag', 'img/bag.jpg');
 new Products('banana', 'img/banana.jpg');
@@ -46,22 +46,22 @@ new Products('water-can', 'img/water-can.jpg');
 new Products('wine-glass', 'img/wine-glass.jpg');
 
 function renderNewProducts() {
-        var forbidden = [Products.firstObject, Products.secondObject,Products.thirdObject];
-      do {
-        Products.firstObject = getRandomProducts();
-      } while (forbidden.includes(Products.firstObject))
-        forbidden.push(Products.firstObject);
-        do {
-            Products.secondObject = getRandomProducts();
-          } while (forbidden.includes(Products.secondObject))
-            forbidden.push(Products.secondObject);
-        do {
-            Products.thirdObject = getRandomProducts();
-      } while(forbidden.includes( Products.thirdObject));
+  var forbidden = [Products.firstObject, Products.secondObject, Products.thirdObject];
+  do {
+    Products.firstObject = getRandomProducts();
+  } while (forbidden.includes(Products.firstObject))
+  forbidden.push(Products.firstObject);
+  do {
+    Products.secondObject = getRandomProducts();
+  } while (forbidden.includes(Products.secondObject))
+  forbidden.push(Products.secondObject);
+  do {
+    Products.thirdObject = getRandomProducts();
+  } while (forbidden.includes(Products.thirdObject));
 
-      Products.firstObject.shownCounter++;
-      Products.secondObject.shownCounter++;
-      Products.thirdObject.shownCounter++;
+  Products.firstObject.shownCounter++;
+  Products.secondObject.shownCounter++;
+  Products.thirdObject.shownCounter++;
 
   var firstProductImageElement = Products.firstImage;
   var secondProductImageElement = Products.secondImage;
@@ -73,86 +73,83 @@ function renderNewProducts() {
   secondProductImageElement.setAttribute('src', Products.secondObject.src);
   secondProductImageElement.setAttribute('alt', Products.secondObject.title);
 
-  thirdProductImageElement.setAttribute('src',  Products.thirdObject.src);
-  thirdProductImageElement.setAttribute('alt',  Products.thirdObject.title);
+  thirdProductImageElement.setAttribute('src', Products.thirdObject.src);
+  thirdProductImageElement.setAttribute('alt', Products.thirdObject.title);
 
 
-  Products.firstImageTitle.textContent =  Products.firstObject.title;
+  Products.firstImageTitle.textContent = Products.firstObject.title;
   Products.secondImageTitle.textContent = Products.secondObject.title;
-  Products.thirdImageTitle.textContent =  Products.thirdObject.title;
+  Products.thirdImageTitle.textContent = Products.thirdObject.title;
 }
 
 function getRandomProducts() {
-    var index = Math.floor(Math.random() * Products.all.length);
-    return Products.all[index];
-  }
-    function randomInRange(min, max) {
-    var range = max - min + 1; 
-    var rand = Math.floor(Math.random() * range) + min
-    return rand;
-  }
+  var index = Math.floor(Math.random() * Products.all.length);
+  return Products.all[index];
+}
+function randomInRange(min, max) {
+  var range = max - min + 1;
+  var rand = Math.floor(Math.random() * range) + min
+  return rand;
+}
 
-  function newTotals() {
+function newTotals() {
 
-    var insideTheTable = document.getElementById('theReport');
-    insideTheTable.innerHTML = '';
-  
-    for (var i = 0; i < Products.all.length; i++) {
-      var product = Products.all[i];
-      var row = addElement('tr', insideTheTable);
-      addElement('td', row, product.title);
-      addElement('td', row, '' + product.clickCounter );
-      addElement('td', row, '' + product.shownCounter );
-    }
+  var insideTheTable = document.getElementById('theReport');
+  insideTheTable.innerHTML = '';
+
+  for (var i = 0; i < Products.all.length; i++) {
+    var product = Products.all[i];
+    var row = addElement('tr', insideTheTable);
+    addElement('td', row, product.title);
+    addElement('td', row, '' + product.clickCounter);
+    addElement('td', row, '' + product.shownCounter);
+    addElement('td', row, '' + ' has (' + product.clickCounter + ') votes and was shown (' + product.shownCounter+ ') times');
+
   }
-  function addElement(tag, container, text) {
-    var element = document.createElement(tag);
-    container.appendChild(element);
-    if(text) {
-      element.textContent = text;
-    }
-    return element;
+}
+function addElement(tag, container, text) {
+  var element = document.createElement(tag);
+  container.appendChild(element);
+  if (text) {
+    element.textContent = text;
   }
+  return element;
+}
 
-  function clickHandler(event) {
+function clickHandler(event) {
 
-    var clickedId = event.target.id;
-    var productClicked;
-  
-    if(clickedId === 'first-image') {
-        productClicked = Products.firstObject;
-      } else if (clickedId === 'second-image') {
-        productClicked = Products.secondObject;
-      } else if (clickedId === 'third-image') {
-        productClicked = Products.thirdObject;
-      } else{
-        console.log('the product you just clicked is', clickedId);
-      }
-      if(productClicked) {
-        productClicked.clickCounter++;
-        Products.roundCounter++;
-    
-        newTotals();
-    
-        if(Products.roundCounter === Products.roundVotingLimit) {
-    
-          alert('your clicking attempts is over!');
-    
-          Products.container.removeEventListener('click', clickHandler);
-    
-        } else {
-    
-          renderNewProducts();
-        }
-      }
-    }
-          Products.container.addEventListener('click', clickHandler);
-    
+  var clickedId = event.target.id;
+  var productClicked;
+
+  if (clickedId === 'first-image') {
+    productClicked = Products.firstObject;
+  } else if (clickedId === 'second-image') {
+    productClicked = Products.secondObject;
+  } else if (clickedId === 'third-image') {
+    productClicked = Products.thirdObject;
+  } else {
+    console.log('the product you just clicked is', clickedId);
+  }
+  if (productClicked) {
+    productClicked.clickCounter++;
+    Products.roundCounter++;
+
     newTotals();
-    
-    renderNewProducts();
-   
-   
-  
-  
-   
+
+    if (Products.roundCounter === Products.roundVotingLimit) {
+      alert('your clicking attempts is over!');
+      Products.container.removeEventListener('click', clickHandler);
+    } else {
+      renderNewProducts();
+    }
+  }
+}
+Products.container.addEventListener('click', clickHandler);
+
+
+newTotals();
+renderNewProducts();
+
+
+
+
